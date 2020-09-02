eb4ad41f16addf9e585ff0eb002c55f02e7d030f
Jonic Linley <jonic@100yen.co.uk>
2020-09-02 18:30:28 +0100

    Make packs js stimulus-aware

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,15 @@
 import "../components";
+
+const application = Application.start();
+const context = require.context("controllers", true, /.js$/);
+const context_components = require.context(
+  "../../components",
+  true,
+  /_controller.js$/
+);
+
+application.load(
+  definitionsFromContext(context).concat(
+    definitionsFromContext(context_components)
+  )
+);
