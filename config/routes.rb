cf5fadc5949e398b99aa66d8adc2a85764936b2f
Jonic Linley <jonic@100yen.co.uk>
2020-09-02 12:29:06 +0100

    Stub application stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources(:example, only: %i[index])
+  resources(:cards, only: %i[index])
+  root("home#index")
 end
