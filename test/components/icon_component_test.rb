6d5d1ae57d9b708bc65901e3e1d9899d95a731d2
Jonic Linley <jonic@100yen.co.uk>
2020-09-02 18:32:35 +0100

    Stub IconComponentTest

---
+++ test/components/icon_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class IconComponentTest < ViewComponent::TestCase
+  test "component renders something useful" do
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(IconComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
