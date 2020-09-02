b1a19daab246af3014c67341e25cd1071e22a89f
Jonic Linley <jonic@100yen.co.uk>
2020-09-02 12:42:44 +0100

    Add section component test

--- test/components/section_component_test.rb
+++ test/components/section_component_test.rb
@@ -1,10 +1,8 @@
 require "test_helper"
 
 class SectionComponentTest < ViewComponent::TestCase
-  test "component renders something useful" do
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(SectionComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  test "component renders contents" do
+    render_inline(SectionComponent.new) { "Whatevs" }
+    assert_selector(".sl-section", text: "Whatevs")
   end
 end
